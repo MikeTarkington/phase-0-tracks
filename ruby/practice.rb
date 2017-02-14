5c2066f92dfd4824217a1cbda02eabf88151c4fd
Mike Tarkington <tarkmike@gmail.com>
2017-02-14 14:02:40 -0800

    A few file updates and completed 4.3 madatory pair.

--- ruby/practice.rb
+++ ruby/practice.rb
@@ -1,1 +1,17 @@
 puts "hello world"
+
+def elevator
+  floor = 0
+  destinations = []
+  add_destination = gets.chomp.to_i
+  
+  #needs to constantly assess against current floor in order to determine next destination
+  current_floor = nil
+  
+  #if it's going up or down will need to do different assessment of current floor
+  direction = nil
+  while
+    
+  end
+  
+end
