76a61814a5d9a30430875f20b8f6c0fbf9ddda2b
Mike Tarkington <tarkmike@gmail.com>
2017-02-20 14:34:14 -0800

    mostly done with mod 5.1 updating method_detective.rb

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,25 +4,35 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+p "iNvEsTiGaTiOn".swapcase
+p "iNvEsTiGaTiOn".swapcase!
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+p "zom".insert(1, "o")
 # => “zoom”
 
-# "enhance".<???>
+p "enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+puts "Stop! You’re under arrest!".upcase
+p "Stop! You’re under arrest!".upcase! #oddly when using p to print it treats
+#the apstrophe rather strangely
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+p "the usual".ljust(18, " suspects")
+p "the usual".insert(9, " suspects")
+p "the usual" + " suspects"
+p "the usual" << " suspects"
 #=> "the usual suspects"
 
-# " suspects".<???>
+p nil
+
+p " suspects".insert(0, "the usual")
+p " suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+p "The case of the disappearing last letter".chomp("r")
+p "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
 # "The mystery of the missing first letter".<???>
@@ -32,7 +42,7 @@
 # => "Elementary, my dear Watson!"
 
 # "z".<???>
-# => 122 
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
 
 # "How many times does the letter 'a' appear in this string?".<???>
