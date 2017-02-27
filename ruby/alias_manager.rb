a28315f3441e2e99e0f791f37150faf7164ab5af
Mike Tarkington <tarkmike@gmail.com>
2017-02-26 17:34:28 -0800

    completed updates to solo challenge 5.5 which get the entire algorthm to work and return statements from a data structure, updated alias_manager.rb

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,19 +2,28 @@
 #1. swapping first and last
 #2. Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
 
+#BUSINESS CODE
+
 def alias_maker(name)
   name_arr = name.split(" ")
   name_arr[0], name_arr[1] = name_arr[1], name_arr[0]
+  #some junk code I didn't need in the end but is relevant to other strategies I
+  #may have taken
   #name_arr.map!(&:capitalize)
-  vowels = "aeiou"
-  vowels_arr = ["a", "e", "i", "o", "u"]
-  consonants = "bcdeghjklmnpqrstvwxyz"
-  idx = 0
-  p alias_first = name_arr[0].chars
+  # vowels = "aeiou"
+  # vowels_arr = ["a", "e", "i", "o", "u"]
+  # consonants = "bcdeghjklmnpqrstvwxyz"
+  # idx = 0
+  alias_first = name_arr[0].chars
   alias_last = name_arr[1].chars
   first_alias_done = []
   last_alias_done = []
 
+#stores converted version of array in a new variable
+#iterates through name as it is borken into an array
+#accounts for vowels being changed to their next vowel in the "aeiou" order with
+#with the edge case of "u" being "a"
+#also accounts for
   alias_first.each do |alias_chr|
     if alias_chr == "a"
      first_alias_done << "e"
@@ -27,14 +36,25 @@ def alias_maker(name)
     elsif alias_chr == "u"
       first_alias_done << "a"
     else
-      unless alias_chr == "z"
-        first_alias_done << alias_chr.next
-      else
+      if alias_chr == "z"
         first_alias_done << "a"
+      elsif alias_chr.next == "e"
+        first_alias_done << alias_chr.next.next
+      elsif alias_chr.next == "i"
+        first_alias_done << alias_chr.next.next
+      elsif alias_chr.next == "o"
+        first_alias_done << alias_chr.next.next
+      elsif alias_chr.next == "u"
+        first_alias_done << alias_chr.next.next
+      else
+        first_alias_done << alias_chr.next
       end
     end
   end
 
+#I know I could refactor to make this one name conversion method instead of writing
+#two separate blocks for it, I just didn't have much time left over and copy/paste
+#involved less thought/effort
   alias_last.each do |alias_chr|
     if alias_chr == "a"
      last_alias_done << "e"
@@ -47,21 +67,49 @@ def alias_maker(name)
     elsif alias_chr == "u"
       last_alias_done << "a"
     else
-      unless alias_chr == "z"
-        last_alias_done << alias_chr.next
-      else
+      if alias_chr == "z"
         last_alias_done << "a"
+      elsif alias_chr.next == "e"
+        last_alias_done << alias_chr.next.next
+      elsif alias_chr.next == "i"
+        last_alias_done << alias_chr.next.next
+      elsif alias_chr.next == "o"
+        last_alias_done << alias_chr.next.next
+      elsif alias_chr.next == "u"
+        last_alias_done << alias_chr.next.next
+      else
+        last_alias_done << alias_chr.next
       end
     end
   end
-p first_alias_done
-p last_alias_done
 return first_alias_done.join("").capitalize + " " + last_alias_done.join("").capitalize
 end
 
-puts "What is the agents name?"
-name = gets.chomp
-p alias_maker(name)
+#USER INTERFACE
+
+status = ""
+stored_aliases = []
+statements = []
+loop do
+  puts "What is the agents first and last name (separated only by a space)?"
+  name = gets.chomp.downcase
+  puts "The alias is: #{alias_maker(name)}"
+  puts "Type \"quit\" if you're done entering names, or hit enter to continue:"
+  status = gets.chomp
+  stored_aliases << alias_maker(name)
+  statements << "Agent #{name} is now aka #{alias_maker(name)}"
+  if status == "quit"
+    break
+  end
+end
+
+puts statements
+
+
+
+
+
+
 
 #initial brainstorms some of which was basically aborted
 =begin
