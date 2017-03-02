1e66aecf8aa7ab3a10d90057260f68aa72ff0995
Mike Tarkington <tarkmike@gmail.com>
2017-03-01 17:28:37 -0800

    completed release 1 steps updating santa.rb with a few iterativeclass instance initialization strategies that supply data to an array

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,11 @@
 class Santa
 
-  def initialize
+  def initialize(gender, ethnicity)
     p "Initializing Santa instance ..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 
   def speak
@@ -14,6 +18,53 @@ class Santa
 
 end
 
-santa0 = Santa.new
+santa0 = Santa.new("agender", "N/A")
 santa0.speak
 santa0.eat_milk_and_cookies("choco chip")
+
+#--DBC EXAMPLE INITIALIZERS--
+# a repl they offer: https://repl.it/CDS5
+
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+
+p santas
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+p santas
+
+#--MY EXAMPLE INITIALIZERS--
+
+idx = 0
+santa_num = nil
+santas = []
+5.times do
+  santa_num = "santa#{idx}"
+  santas << santa_num
+  santas << Santa.new("female", "Korean")
+  idx += 1
+end
+
+p santas
+
+santa_num = nil
+santas = []
+example_ethnicities.length.times do |i|
+  santa_num = "santa#{i}"
+  santas << santa_num
+  santas << Santa.new("male", "Canadian")
+end
+
+p santas
