709281f120496bdc6a9e96487568730607287653
Mike Tarkington <tarkmike@gmail.com>
2017-03-18 19:41:28 -0700

    the program now has full capability to create a new user in the db after receivi ng inputs and calculating their projected political stance. next step is to offer opportnities to  update their inputs and to query the data with use of foreign key queries

--- databases/political_stance/political_stance.rb
+++ databases/political_stance/political_stance.rb
@@ -81,12 +81,12 @@ db.execute(create_quiz_results_table_cmd)
 
 # create quiz result political stances(RAN IT ONCE)
 # db.execute("INSERT INTO quiz_results (result) VALUES ('far left liberal')") #=> 1
-# db.execute("INSERT INTO quiz_results (result) VALUES ('liberal')") #=> 1
-# db.execute("INSERT INTO quiz_results (result) VALUES ('moderate liberal')") #=>
-# db.execute("INSERT INTO quiz_results (result) VALUES ('moderate')") #=> 1
-# db.execute("INSERT INTO quiz_results (result) VALUES ('moderate conservative')") #=> 1
-# db.execute("INSERT INTO quiz_results (result) VALUES ('conservative')") #=> 1
-# db.execute("INSERT INTO quiz_results (result) VALUES ('far right conservative')") #=> 1
+# db.execute("INSERT INTO quiz_results (result) VALUES ('liberal')") #=> 2
+# db.execute("INSERT INTO quiz_results (result) VALUES ('moderate liberal')") #=> 3
+# db.execute("INSERT INTO quiz_results (result) VALUES ('moderate')") #=> 4
+# db.execute("INSERT INTO quiz_results (result) VALUES ('moderate conservative')") #=> 5
+# db.execute("INSERT INTO quiz_results (result) VALUES ('conservative')") #=> 6
+# db.execute("INSERT INTO quiz_results (result) VALUES ('far right conservative')") #=> 7
 
 
 # add fake users(RAN IT ONCE)
@@ -113,18 +113,87 @@ db.execute(create_quiz_results_table_cmd)
 def stance_determinator(answer_1, answer_2, answer_3, answer_4, answer_5)
   left = 0
   right = 0
-  if answer_id_1
+  political_stance = 0
+
+  if answer_1 == 1
+    left += 3
+  elsif answer_1 == 2
+    right += 3
+  elsif answer_1 == 3
+    left += 2
+  elsif answer_1 == 4
+    right += 2
+  end
+
+  if answer_2 == 1
+    left += 3
+  elsif answer_2 == 2
+    right += 3
+  elsif answer_2 == 3
+    left += 2
+  elsif answer_2 == 4
+    right += 2
+  end
+
+  if answer_3 == 1
+    right += 3
+  elsif answer_3 == 2
+    left += 3
+  elsif answer_3 == 3
+    right += 2
+  elsif answer_3 == 4
+    left += 2
+  end
+
+  if answer_4 == 1
+    right += 3
+  elsif answer_4 == 2
+    left += 3
+  elsif answer_4 == 3
+    right += 2
+  elsif answer_4 == 4
+    left += 2
+  end
+
+  if answer_5 == 1
+    left += 3
+  elsif answer_5 == 2
+    right += 3
+  elsif answer_5 == 3
+    left += 2
+  elsif answer_5 == 4
+    right += 2
+  end
+
+  puts "You have #{left} points toward liberalism and #{right} points toward conservatism."
+
+  if left <= 1
+    political_stance = 7
+  elsif left >= 2 && left <= 4
+    political_stance = 6
+  elsif left >= 5 && left <= 6
+    political_stance = 5
+  elsif left >= 7 && left <= 8
+    political_stance = 4
+  elsif left >= 9 && left <= 10
+    political_stance = 3
+  elsif left >= 11 && left <= 13
+    political_stance = 2
+  elsif left >= 14 && left <= 15
+    political_stance = 1
+  end
+
 end
 
-def create_user(db, name, email, password, answer_id_1, answer_id_2, answer_id_3, answer_id_4, answer_id_5, political_stance)
-  db.execute("INSERT INTO #{users} (name, email, password, answer_id_1, answer_id_2, answer_id_3, answer_id_4, answer_id_5, political_stance) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", name, email, password, answer_id_1, answer_id_2, answer_id_3, answer_id_4, answer_id_5, political_stance)
+def create_user(db, name, email, password, answer_1, answer_2, answer_3, answer_4, answer_5, political_stance)
+  db.execute("INSERT INTO users (name, email, password, answer_id_1, answer_id_2, answer_id_3, answer_id_4, answer_id_5, political_stance) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", name, email, password, answer_1, answer_2, answer_3, answer_4, answer_5, political_stance)
 end
 
 # --USER INTERFACE--
 
 puts "Welcome to the mini political position assement app!"
 puts " "
-puts "Enter the basics to create your account and then answer a hadful of quick questions to start getting a sense of your true political standing.  Obviously ones views and political opinions are too complex to be summed up in 5 simple questions, but it's a good start on our journey toward better citizenship."
+puts "Enter the basics to create your account and then answer a handful of quick questions to start getting a sense of your true political standing on a spectrum of liberalism to conservatism.  Obviously ones views and political opinions are too complex to be summed up in 5 simple questions, but it's a good start on our journey toward better citizenship and self understanding."
 puts " "
 puts "What is your name?"
 name = gets.chomp
@@ -147,24 +216,27 @@ questions = db.execute("SELECT * FROM questions")
 puts "Question 1:"
 puts questions[0][1]
 puts "Your answer number:"
-answer_1 = gets.chomp
+answer_1 = gets.chomp.to_i
 
 puts "Question 2:"
 puts questions[1][1]
 puts "Your answer number:"
-answer_2 = gets.chomp
+answer_2 = gets.chomp.to_i
 
 puts "Question 3:"
 puts questions[2][1]
 puts "Your answer number:"
-answer_3 = gets.chomp
+answer_3 = gets.chomp.to_i
 
 puts "Question 4:"
 puts questions[3][1]
 puts "Your answer number:"
-answer_4 = gets.chomp
+answer_4 = gets.chomp.to_i
 
 puts "Question 5:"
 puts questions[4][1]
 puts "Your answer number:"
-answer_5 = gets.chomp
+answer_5 = gets.chomp.to_i
+
+p political_stance = stance_determinator(answer_1, answer_2, answer_3, answer_4, answer_5)
+create_user(db, name, email, password, answer_1, answer_2, answer_3, answer_4, answer_5, political_stance)
